bd3b094064cba2f93f7d7d91308d068ede0986c3
lindberg.junior <lindberg_junior@hotmail.com>
2018-04-15 12:20:42 -0300

    att

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+import { Studant } from './studant/studant.model'
+
 @Component({
   selector: 'jad-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'jad';
+  // Propriedade no plaral para representar todos esses estudantes, que é um array de estudantes
+  studants: Studant[] = [
+    {name: 'Luke', isJedi: true, temple: 'Coruscant'},
+    {name: 'Leia', isJedi: false},
+    {name: 'Han', isJedi: false}
+  ]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { StudantComponent } from './studant/studant.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StudantComponent
   ],
   imports: [
     BrowserModule
